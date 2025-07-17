4b940c582dca114c3abffaabfde70a090e89d6c5
Costin <costin@me.com>
2025-07-17 13:56:29 -0400

    Add workout components

--- frontend/src/components/Exercise.vue
+++ frontend/src/components/Exercise.vue
@@ -1,148 +1,29 @@
 <template>
-  <v-container class="py-8">
-    <v-card class="pa-6 rounded-xl" elevation="6" color="blue-grey-lighten-5">
-      <v-card-title class="text-h5 font-weight-bold text-blue">🏋️‍♂️ Create an Exercise</v-card-title>
+    
+    <h1>{{ exercise.exercise_def.name }}</h1>
+    <p>{{ exercise.note }}</p>
 
-      <v-divider class="my-4"></v-divider>
-
-      <v-form @submit.prevent="addExercise" class="d-flex flex-column gap-4">
-        <v-text-field
-          v-model="exerciseName"
-          label="Exercise Name"
-          variant="outlined"
-          color="blue"
-          required
-        />
-
-        <v-text-field
-          v-model="exerciseDesc"
-          label="Description"
-          variant="outlined"
-          color="blue"
-        />
-
-        <v-select
-          v-model="bodyParts"
-          :items="bodyPartsList"
-          label="Body Parts"
-          variant="outlined"
-          color="orange"
-          multiple
-          chips
-        />
-
-        <v-btn
-          type="submit"
-          color="orange-darken-2"
-          class="mt-2"
-          size="large"
-          rounded
-          elevation="2"
-        >
-          ➕ Add Exercise
-        </v-btn>
-      </v-form>
-    </v-card>
-
-    <v-card
-      v-if="exerciseList.length"
-      class="mt-10 pa-6 rounded-xl"
-      elevation="4"
-      color="indigo-lighten-5"
-    >
-      <v-card-title class="text-h6 font-weight-bold text-indigo-darken-2">
-        📋 Exercise List to add
-      </v-card-title>
-      <v-divider class="my-3"></v-divider>
-      <v-list density="comfortable">
-        <v-list-item
-          v-for="(ex, index) in exerciseList"
-          :key="index"
-          class="rounded-lg my-2"
-        >
-        <v-list-item-content>
-          <v-list-item-title class="font-weight-bold text-blue-darken-3">
-            {{ ex.name }}
-          </v-list-item-title>
-
-          <div class="d-flex justify-space-between align-center">
-            <div class="text-body-2">
-              {{ ex.description }} <br />
-              <span class="text-orange-darken-2">
-                ({{ ex.bodyParts.join(', ') }})
-              </span>
-            </div>
-
-            <v-icon icon="mdi-delete" size="30" class="text-red-darken-2 cursor-pointer" @click="confirmDelete(index)"></v-icon>
-          </div>
-        </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-card>
-    <v-dialog v-model="dialog" max-width="400">
-      <v-card>
-        <v-card-title class="text-h6">
-          Confirm deleting
-        </v-card-title>
-
-        <v-card-text>
-          Are you sure you want to delete this exercise?
-        </v-card-text>
-
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="grey" text @click="cancelDelete">Cancel</v-btn>
-          <v-btn color="red" text @click="deleteExercise">Delete</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-  </v-container>
+    <v-table>
+        <thead>
+          <tr>
+            <th>Type</th>
+            <th>Note</th>
+            <th>Reps</th>
+            <th>Weight (kg)</th>
+          </tr>
+        </thead>
+        <tbody>
+            <Set v-for="set in exercise.sets" :set="set"></Set>
+        </tbody>
+    </v-table>
 </template>
 
 <script setup>
-import { ref, shallowRef } from 'vue'
-
-const exerciseName = ref('')
-const exerciseDesc = ref('')
-const exerciseList = ref([])
-
-const bodyPartsList = ["Back", "Chest", "Legs", "Arms", "Shoulders"]
-const bodyParts = shallowRef([])
-
-const dialog = ref(false);
-const exerciseToDeleteIndex = ref(null);
-
-function addExercise() {
-  exerciseList.value.push({
-    name: exerciseName.value,
-    description: exerciseDesc.value,
-    bodyParts: [...bodyParts.value]
-  })
-
-  exerciseName.value = ''
-  exerciseDesc.value = ''
-  bodyParts.value = []
-}
-
-function confirmDelete (index) {
-  exerciseToDeleteIndex.value = index;
-  dialog.value = true;
-}
-
-function deleteExercise(){
-  if (exerciseToDeleteIndex.value !== null) {
-    exerciseList.value.splice(exerciseToDeleteIndex.value, 1);
-    exerciseToDeleteIndex.value = null;
-    dialog.value = false;
+import Set from './Set.vue'
+defineProps({
+  exercise: {
+    type: Object,
+    required: true,
   }
-}
-
-function cancelDelete(){
-  exerciseToDeleteIndex.value = null;
-  dialog.value = false;
-}
-
-defineExpose({
-  exerciseList
 })
 </script>
---
+++ frontend/src/components/Set.vue
@@ -0,0 +1,17 @@
+<template>
+    <tr :key="set.set_id">
+        <td>{{ set.set_type }}</td>
+        <td>{{ set.note || '-' }}</td>
+        <td>{{ set.repetitions }}</td>
+        <td>{{ set.weight }}</td>
+    </tr>
+</template>
+
+<script setup>
+defineProps({
+  set: {
+    type: Object,
+    required: true,
+  }
+})
+</script>

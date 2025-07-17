153fa6dc27b1ce991bba961b5b870e32463cf91e
Theo <theo.gasnier.49@gmail.com>
2025-07-17 09:22:18 -0400

    front to add exercise + main view added

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,24 @@
 <script setup>
-import LoginView from './views/LoginView.vue'
+import Exercise from './components/Exercise.vue';
+import HelloWorld from './components/HelloWorld.vue'
+import MainPage from './components/MainPage.vue';
+import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
+  <!-- <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
   </header>
 
   <main>
-    <LoginView/>
-  </main>
+    <TheWelcome />
+  </main> -->
+  <v-app>
+    <MainPage/>
+  </v-app>
+  
 </template>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,7 +4,23 @@ import {createApp} from 'vue'
 import App from './App.vue'
 import router from "./routeur";
 
+// Import Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+// (Optionnel) Icônes Material Design
+import '@mdi/font/css/materialdesignicons.css'
+
+// Création instance Vuetify
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+// Création app Vue
 createApp(App)
-    .use(router)
-    .mount('#app')
+  .use(vuetify)
+  .mount('#app')
 
